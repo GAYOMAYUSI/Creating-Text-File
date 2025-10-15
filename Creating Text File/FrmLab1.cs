96feccabe774a5f1918d4c6ef8eb1c92e8625be1
Gayoma <Gayoma@GAYOMAYUSI>
2025-10-15 19:52:15 +0800

    Add project files.

---
+++ Creating Text File/FrmLab1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Creating_Text_File
+{
+    public partial class FrmLab1 : Form
+    {
+        public FrmLab1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
---
+++ Creating Text File/FrmRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Creating_Text_File
+{
+    public partial class FrmRegistration : Form
+    {
+        public FrmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
