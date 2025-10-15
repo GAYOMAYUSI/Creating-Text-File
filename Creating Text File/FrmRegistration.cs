384a0d27d0bdcace9c2876e689200bb02c196073
Gayoma <Gayoma@GAYOMAYUSI>
2025-10-15 19:54:39 +0800

    Commiting The to create the code in frm registration

--- Creating Text File/FrmRegistration.cs
+++ Creating Text File/FrmRegistration.cs
@@ -20,7 +20,23 @@ namespace Creating_Text_File
 
         private void btnRegister_Click(object sender, EventArgs e)
         {
-          
+            String[] value =
+            {
+                txtStudNo.Text,
+                txtFirstName.Text +" "+txtMI.Text+" "+txtLastName.Text,
+                cbProgram.Text,
+                cbGender.Text,
+                txtAge.Text,
+                dtpBirthday.Value.ToString("yyyy-mm-dd"),
+                txtContact.Text
+            };
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, txtStudNo.Text+".txt")))
+            {
+                outputFile.Write("Student No.: " + value[0] +"\nFull Name: "+ value[1]
+                    +"\nProgram: " + value[2]+"\nGender: " + value[3]+"\nAge: " + value[4]
+                    +"\nBirthday: " + value[5]+"\nContact No.: " + value[6]);
+            }
         }
     }
 }
